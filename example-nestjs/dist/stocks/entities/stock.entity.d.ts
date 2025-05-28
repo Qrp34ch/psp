6f1df0bcc1ca5b8380daed5e36f5a3578f2c81a8
Qrp34ch <168339978+Qrp34ch@users.noreply.github.com>
2025-05-28 13:41:30 +0300

    Add files via upload

---
+++ example-nestjs/dist/stocks/entities/stock.entity.d.ts
@@ -0,0 +1,9 @@
+export declare class Stock {
+    id: number;
+    src: string;
+    title: string;
+    zp: number;
+    text: string;
+    detali: string;
+    code: number;
+}
---
+++ example-nestjs/dist/stocks/entities/stock.entity.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Stock = void 0;
+class Stock {
+    id;
+    src;
+    title;
+    zp;
+    text;
+    detali;
+    code;
+}
+exports.Stock = Stock;
+//# sourceMappingURL=stock.entity.js.map
