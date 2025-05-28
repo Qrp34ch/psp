a1b84cf99584cd8de51b6ab30e2950b43902616b
Qrp34ch <168339978+Qrp34ch@users.noreply.github.com>
2025-05-28 13:39:38 +0300

    Add files via upload

---
+++ example-nestjs/dist/stocks/stocks.service.d.ts
@@ -0,0 +1,13 @@
+import { CreateStockDto } from './dto/create-stock.dto';
+import { UpdateStockDto } from './dto/update-stock.dto';
+import { Stock } from './entities/stock.entity';
+import { FileService } from 'src/file.service';
+export declare class StocksService {
+    private fileService;
+    constructor(fileService: FileService<Stock[]>);
+    create(createStockDto: CreateStockDto): void;
+    findAll(title?: string): Stock[];
+    findOne(id: number): Stock | null;
+    update(id: number, updateStockDto: UpdateStockDto): void;
+    remove(id: number): void;
+}
